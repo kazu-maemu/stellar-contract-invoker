ff23b5ff2a5accbf29ae8849f926c81ba840ab94
kazu-maemu <kazu.maemu@gmail.com>
2025-03-12 20:26:17 +0000

    feat: integrate-functions-to-function-editor

--- frontend/src/components/FunctionEditor.tsx
+++ frontend/src/components/FunctionEditor.tsx
@@ -1,71 +1,125 @@
-import React, { useState } from 'react';
 import {
   Box,
   Button,
-  Input,
-  VStack,
   FormLabel,
+  Grid,
+  GridItem,
   HStack,
-  useQuery
+  Input,
+  Text,
+  Textarea,
+  VStack
 } from '@chakra-ui/react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../App';
+import { Func } from '../types';
+import axios from '../utils/axios';
 
 export const FunctionEditor = () => {
-  const [parameters, setParameters] = useState([{ key: '', value: '' }]);
-  const [functionName, setFunctionName] = useState('');
+  const queryClient = useQueryClient();
+  const { contractId, account } = useContext(AppContext);
 
-  const addParameter = () => {
-    setParameters([...parameters, { key: '', value: '' }]);
-  };
+  const { data } = useQuery({
+    queryKey: ['func'],
+    queryFn: () => axios.get<Func[]>('/func'),
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 
-  const updateParameter = (index: number, field: 'key' | 'value', value: string) => {
-    const newParameters = [...parameters];
-    newParameters[index] = { ...newParameters[index], [field]: value };
-    setParameters(newParameters);
-  };
+  const funcs = data?.data ?? [];
 
-  const removeParameter = (index: number) => {
-    setParameters(parameters.filter((_, i) => i !== index));
-  };
+  const [func, setFunc] = useState<Func | undefined>(undefined);
+  const [name, setName] = useState('');
+  const [params, setParams] = useState('');
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+    if (func) {
+      setName(func.name);
+      setParams(func.params);
+    } else {
+      setName('');
+      setParams('');
+    }
+  }, [func]);
+
+  const handleSave = async () => {
+    if (func) {
+      await axios.patch(`/func/${func.id}`, { name, params });
+    } else {
+      await axios.post(`/func`, { name, params });
+    }
+    queryClient.invalidateQueries({ queryKey: ['func'] });
+  }
+
+  const handleExecute = async () => {
+    if (!account)
+      throw new Error('Please select account to sign transaction');
+    const command = `soroban contract invoke \ --id ${contractId} \ --source-account ${account.sec_key} \ --network testnet \ -- ${name} ${params ? `\ ${params.replace('{{OWNER}}', account.pub_key)}` : ''}`;
+    const { data: { stdout, stderr } } = await axios.post(`/contract/run`, { command });
+    const prettyStdout = ((stdout) => {
+      try {
+        return `${JSON.stringify(JSON.parse(stdout), null, 2)}\n`;
+      } catch (err) {
+        return stdout;
+      }
+    })(stdout);
+    setResult(`${prettyStdout}${stderr}`);
+  }
 
   return (
-    <Box p={4} borderWidth="1px" borderRadius="lg" bg="white" shadow="sm">
-      <VStack spacing={4} align="stretch">
-        <Box>
-          <FormLabel>Function Name</FormLabel>
-          <Input
-            value={functionName}
-            onChange={(e) => setFunctionName(e.target.value)}
-            placeholder="Enter function name"
-          />
-        </Box>
+    <Grid templateColumns="repeat(12, 1fr)" gap={4} p={4} borderWidth="1px" borderRadius="lg" bg="white" shadow="sm">
+      <GridItem colSpan={4}>
+        <VStack spacing={2} align="stretch">
+          {funcs.map((func: any) => (
+            <HStack
+              key={func.id}
+              p={2}
+              _hover={{ bg: 'gray.50' }}
+              borderRadius="md"
+              cursor='pointer'
+              onClick={() => setFunc(func)}
+            >
+              <Text flex={1}>{func.name}</Text>
+              <Button
+                size="sm"
+              >
+              </Button>
+            </HStack>
+          ))}
+          <Button onClick={() => setFunc(undefined)}>
+            Add new
+          </Button>
+        </VStack>
+      </GridItem>
+      <GridItem colSpan={8}>
+        <VStack spacing={4} align="stretch">
+          <Box>
+            <FormLabel>Function Name</FormLabel>
+            <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter function name"
+            />
+          </Box>
 
-        <Box>
-          <FormLabel>Parameters</FormLabel>
-          <VStack spacing={2}>
-            {parameters.map((param, index) => (
-              <HStack key={index}>
-                <Input
-                  value={param.key}
-                  onChange={(e) => updateParameter(index, 'key', e.target.value)}
-                  placeholder="Key"
-                />
-                <Input
-                  value={param.value}
-                  onChange={(e) => updateParameter(index, 'value', e.target.value)}
-                  placeholder="Value"
-                />
-                <Button onClick={() => removeParameter(index)}>Remove</Button>
-              </HStack>
-            ))}
-          </VStack>
-          <Button mt={2} onClick={addParameter}>Add Parameter</Button>
-        </Box>
+          <Box>
+            <FormLabel>Parameters</FormLabel>
+            <Textarea minH={40} value={params} onChange={(e) => setParams(e.target.value)} />
+          </Box>
 
-        <HStack justify="flex-end" spacing={2}>
-          <Button>Save Function</Button>
-          <Button colorScheme="blue">Execute</Button>
-        </HStack>
-      </VStack>
-    </Box>
+          <Box>
+            <FormLabel>Result</FormLabel>
+            <Textarea minH={40} value={result} readOnly />
+          </Box>
+
+          <HStack justify="flex-end" spacing={2}>
+            <Button onClick={handleSave}>Save Function</Button>
+            <Button colorScheme="blue" onClick={handleExecute}>Execute</Button>
+          </HStack>
+        </VStack>
+      </GridItem>
+    </Grid>
   );
 };
