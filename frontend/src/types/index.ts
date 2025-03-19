2b06a5af13313196d30962aed102b3fc41909efd
kazu-maemu <kazu.maemu@gmail.com>
2025-03-19 18:56:12 +0000

    feat: add param interface

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,18 +1,35 @@
-export interface Contract {
-  id: string;
+export interface IContract {
+  id: number;
   name: string;
   contract_id: string;
 }
 
-export interface KeyPair {
-  id: string;
+export interface IKeyPair {
+  id: number;
   name: string;
   pub_key: string;
   sec_key: string;
 }
 
-export interface Func {
-  id: string;
+export interface IFunc {
+  id: number;
   name: string;
   params: string;
+}
+
+export enum ParamType {
+  Array = "array",
+  JSON = "json",
+  PublicKey = "public_key",
+  Contract = "contract",
+  String = "string",
+  Number = "number",
+}
+
+export interface IParam {
+  id: string;
+  type: ParamType;
+  key: string;
+  value: string;
+  children?: IParam[];
 }
