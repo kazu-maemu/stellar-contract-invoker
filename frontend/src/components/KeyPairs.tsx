7810758dd52d4ddb8a17c1a0f23e30fce6a4124e
kazu-maemu <kazu.maemu@gmail.com>
2025-03-20 08:17:09 +0000

    feat: update keypair component

--- frontend/src/components/KeyPairs.tsx
+++ frontend/src/components/KeyPairs.tsx
@@ -8,36 +8,49 @@ import {
   Text,
   VStack
 } from '@chakra-ui/react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import React, { useContext, useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
+import useKeyPair from '../api/useKeypair';
 import { AppContext } from '../App';
-import { KeyPair } from '../types';
+import { IKeyPair } from '../types';
 import axios from '../utils/axios';
 
 export const KeyPairs = () => {
   const queryClient = useQueryClient();
-  const { setAccount } = useContext(AppContext);
-
-  const { data } = useQuery({
-    queryKey: ['key-pair'],
-    queryFn: () => axios.get<KeyPair[]>('/key-pair'),
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-  });
-
-  const keyPairs = data?.data ?? [];
+  const { account, setAccount } = useContext(AppContext);
+  const { keyPairs } = useKeyPair();
 
   const [name, setName] = useState('');
   const [pubKey, setPubKey] = useState('');
   const [secKey, setSecKey] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleAdd = async () => {
-    await axios.post(`/key-pair`, {
-      name,
-      pub_key: pubKey,
-      sec_key: secKey,
-    });
-    queryClient.invalidateQueries({ queryKey: ['key-pair'] });
+    setIsAdding(true);
+    try {
+      await axios.post(`/key-pair`, {
+        name,
+        pub_key: pubKey,
+        sec_key: secKey,
+      });
+      queryClient.invalidateQueries({ queryKey: ['key-pair'] });
+    } catch (error) {
+      console.error('There was an error adding the key pair:', error);
+    }
+    setIsAdding(false);
+  }
+
+  const handleDelete = async (id: number) => {
+    setIsDeleting(true);
+    try {
+      await axios.delete(`/key-pair/${id}`);
+      queryClient.invalidateQueries({ queryKey: ['key-pair'] });
+    } catch (error) {
+      console.error('There was an error deleting the key pair:', error);
+    }
+    setIsDeleting(false);
   }
 
   return (
@@ -61,25 +74,30 @@ export const KeyPairs = () => {
           </FormLabel>
           <Input value={secKey} onChange={(e) => setSecKey(e.target.value)} />
         </Box>
-        <Button onClick={handleAdd}>
-          Add new
+        <Button colorScheme="green" onClick={handleAdd} isLoading={isAdding}>
+          Add
         </Button>
       </VStack>
       <Heading size="md" mb={4}>Key pairs</Heading>
       <VStack spacing={2} align="stretch">
-        {keyPairs.map((keyPair: KeyPair) => (
+        {keyPairs.map((keyPair: IKeyPair) => (
           <HStack
             key={keyPair.id}
             p={2}
             _hover={{ bg: 'gray.50' }}
+            {...(account?.id === keyPair.id ? { bg: 'gray.100' } : {})}
             borderRadius="md"
             cursor='pointer'
             onClick={() => setAccount?.(keyPair)}
           >
             <Text flex={1}>{keyPair.name}</Text>
             <Button
+              onClick={() => handleDelete(keyPair.id)}
+              isLoading={isDeleting}
+              colorScheme="red"
               size="sm"
             >
+              <FaTrash />
             </Button>
           </HStack>
         ))}
