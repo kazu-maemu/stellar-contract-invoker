7b535091fd0afb613c4f1751d51c5795a36a5796
kazu-maemu <kazu.maemu@gmail.com>
2025-03-20 08:16:00 +0000

    feat: add modals to edit contract

---
+++ frontend/src/components/AddContractModal.tsx
@@ -0,0 +1,82 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react';
+import { useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import axios from '../utils/axios';
+
+const AddContractModal: React.FC = () => {
+  const queryClient = useQueryClient();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [contractName, setContractName] = useState('');
+  const [contractAddress, setContractAddress] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+
+  const handleAddContract = async () => {
+    setIsAdding(true);
+    try {
+      await axios.post('/contract', { name: contractName, contract_id: contractAddress });
+      queryClient.invalidateQueries({ queryKey: ['contract'] });
+      onClose();
+    } catch (error) {
+      console.error('There was an error adding the contract:', error);
+    }
+    setIsAdding(false);
+  };
+
+  return (
+    <>
+      <Button colorScheme='green' onClick={onOpen}>
+        <FaPlus />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Contract</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl id="contract-name" isRequired>
+              <FormLabel>Contract Name</FormLabel>
+              <Input
+                value={contractName}
+                onChange={(e) => setContractName(e.target.value)}
+                placeholder="Enter contract name"
+              />
+            </FormControl>
+
+            <FormControl id="contract-address" isRequired mt={4}>
+              <FormLabel>Contract Address</FormLabel>
+              <Input
+                value={contractAddress}
+                onChange={(e) => setContractAddress(e.target.value)}
+                placeholder="Enter contract address"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="green" mr={3} onClick={handleAddContract} isLoading={isAdding}>
+              Add Contract
+            </Button>
+            <Button variant="ghost" onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AddContractModal;
---
+++ frontend/src/components/EditContractModal.tsx
@@ -0,0 +1,85 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useQueryClient } from '@tanstack/react-query';
+import React, { FC, useEffect, useState } from 'react';
+import { FaEdit } from 'react-icons/fa';
+import { IContract } from '../types';
+import axios from '../utils/axios';
+
+interface Props {
+  contract: IContract;
+}
+
+const EditContractModal: FC<Props> = ({ contract }) => {
+  const queryClient = useQueryClient();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState('');
+  const [contractId, setContractId] = useState('');
+
+  useEffect(() => {
+    setName(contract.name);
+    setContractId(contract.contract_id);
+    return () => { };
+  }, [contract]);
+
+  const handleEdit = async () => {
+    setIsEditing(true);
+    try {
+      await axios.patch(`/contract/${contract.id}`, { name, contract_id: contractId });
+      queryClient.invalidateQueries({ queryKey: ['contract'] });
+      onClose();
+    } catch (error) {
+      console.error('There was an error editing the contract:', error);
+    }
+    setIsEditing(false);
+  };
+
+  return (
+    <>
+      <Button colorScheme='blue' onClick={onOpen}>
+        <FaEdit />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Contract</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Contract Name</FormLabel>
+              <Input placeholder="Enter contract name" value={name} onChange={(e) => setName(e.target.value)} />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Contract Details</FormLabel>
+              <Input placeholder="Enter contract details" value={contractId} onChange={(e) => setContractId(e.target.value)} />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" isLoading={isEditing} onClick={handleEdit}>
+              Save
+            </Button>
+            <Button variant="ghost" onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default EditContractModal;
